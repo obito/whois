67acd814d396b82f96f6b0ff7cc1ff51b4f3ea5a
youssef <64253722+obito@users.noreply.github.com>
2020-06-06 02:14:47 +0200

    init

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/obito/whois
+
+go 1.14
---
+++ main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net"
+	"os"
+	"regexp"
+	"strings"
+	"time"
+)
+
+func main() {
+	domains := os.Args[1:]
+
+	for _, domain := range domains {
+		extSplited := strings.Split(domain, ".")
+		ext := extSplited[len(extSplited)-1]
+		log.Print(fmt.Sprintf("# whois.nic.%s", ext))
+		result, err := query(fmt.Sprintf("whois.nic.%s", ext), domain)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Print(result)
+
+		whoisServer := server(result)
+
+		log.Print("# " + whoisServer)
+
+		result, err = query(whoisServer, domain)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		log.Print(result)
+	}
+}
+
+func query(server, domain string) (string, error) {
+
+	conn, err := net.DialTimeout("tcp", net.JoinHostPort(server, "43"), time.Second*30)
+	if err != nil {
+		return "", fmt.Errorf("connetion failed: %v", err)
+	}
+
+	defer conn.Close()
+
+	conn.SetWriteDeadline(time.Now().Add(time.Second * 30))
+	_, err = conn.Write([]byte(domain + "\r\n"))
+	if err != nil {
+		return "", fmt.Errorf("sending failed: %v", err)
+	}
+
+	conn.SetReadDeadline(time.Now().Add(time.Second * 30))
+	buff, err := ioutil.ReadAll(conn)
+	if err != nil {
+		return "", fmt.Errorf("read failed: %v", err)
+	}
+
+	return string(buff), nil
+}
+
+func server(result string) string {
+	var reg = regexp.MustCompile(`Registrar WHOIS Server: (.*)`)
+
+	return strings.SplitN(reg.FindString(result), ": ", 2)[1]
+}
